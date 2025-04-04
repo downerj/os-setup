a7e4f681c80a8a01eb3beb45088697d4551ba72a
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-04-03 20:27:46 -0600

    Go Jupyter kernel setup

--- Ubuntu/Scripts/install-Jupyter-Kernels.sh
+++ Ubuntu/Scripts/install-Jupyter-Kernels.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+# Note: Make sure Jupyter is installed and each language's path setup script
+# is run before this one.
+
+kernels="$(jupyter --data-dir)/kernels"
+
 #
 # Go.
+# https://github.com/gopherdata/gophernotes
 #
+go install github.com/gopherdata/gophernotes@v0.7.5
+mkdir -p "$kernels/gophernotes"
+pushd "$kernels/gophernotes"
+cp "$(go env GOPATH)/pkg/mod/github.com/gopherdata/gophernotes@v0.7.5/kernel/*" ./
+chmod +w kernel.json
+sed "s|gophernotes|$(go env GOPATH)/bin/gophernotes|" < kernel.json.in > kernel.json
+popd
 
 #
 # Java.
@@ -27,3 +40,5 @@
 #  ijavascript \
 #  itypescript
 #.local/share/npm_user/bin/ijsinstall --install=local
+
+unset kernels
