f327ee13d4e86a1ea49e15194040a93b3e7cb5bd
James Downer <jamesdanieldowner@gmail.com>
2025-07-21 00:34:14 -0600

    Apt packages script for Termux

---
+++ Android/Termux/Scripts/install-Apt-Packages.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+declare -a apt_apps
+apt_apps=(\
+  clang\
+  coreutils\
+  curl\
+  dnsutils\
+  dos2unix\
+  emacs\
+  ffmpeg\
+  game-music-emu\
+  gdb\
+  git\
+  imagemagick\
+  json-c\
+  lynx\
+  make\
+  mandoc\
+  manpages\
+  nano\
+  ncurses\
+  ncurses-utils\
+  neofetch\
+  nodejs\
+  openssh\
+  python\
+  python-pip\
+  tmux\
+  tree\
+  vim\
+  xz-utils\
+)
+
+echo -e "Selected Apt apps to install:\n${apt_apps[*]}"
+pkg install ${apt_apps[@]}
