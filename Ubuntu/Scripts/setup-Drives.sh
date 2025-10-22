600a611d2e31dea3b9693cf3c60303c6a19282f7
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-10-22 10:14:29 -0600

    Ubuntu drive setup umask=000

--- Ubuntu/Scripts/setup-Drives.sh
+++ Ubuntu/Scripts/setup-Drives.sh
@@ -8,4 +8,5 @@ ls -o /dev/disk/by-uuid/
 
 # Copy the following to the end of /etc/fstab, replacing $UUID with the UUID found in the previous step,
 # and replace $USER with the name of the user (i.e. `echo $USER`).
-#/dev/disk/by-uuid/$UUID /media/$USER/Data auto rw,nosuid,nodev,relatime,uid=1000,gid=1000,iocharset=utf8,noauto,x-gvfs-name=Data,x-gvfs-show 0 0
+# Note: might be able to repalce umask=000 with dmask=000,fmask=111 (?).
+#/dev/disk/by-uuid/$UUID /media/$USER/Data auto rw,nosuid,nodev,relatime,uid=1000,gid=1000,umask=000,iocharset=utf8,noauto,x-gvfs-name=Data,x-gvfs-show 0 0
