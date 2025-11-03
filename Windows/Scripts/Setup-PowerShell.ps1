faeb0256a9ec2d1c0154861ba1b16a21f11cd1b8
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-11-03 00:30:10 -0700

    Setup-PowerShell.ps1

---
+++ Windows/Scripts/Setup-PowerShell.ps1
@@ -0,0 +1,7 @@
+# Note: the contents of this file must be run manually in a PowerShell window.
+
+# To allow running PowerShell scripts for the current user, run the following in a regular PowerShell window:
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
+# To allow running PowerShell scripts for all users, run the following in an elevated/administrator PowerShell window:
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
